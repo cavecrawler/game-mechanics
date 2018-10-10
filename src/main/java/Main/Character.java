8574497fb0486ca4c63119cefc0c0e7c8208bb3c
cydietz <cy.dietz@googlemail.com>
2018-10-10 02:42:58 +0200

    - random character creation v.01

--- src/main/java/Main/Character.java
+++ src/main/java/Main/Character.java
@@ -2,12 +2,53 @@ package Main;
 
 public class Character {
 
-    private String charGender;
     private String charName;
-    private String charType;
+    private String charGender;
+    private CharacterClass characterClass;
     private int hitpoints;
 
-    public Character() {
-
+    public Character(String charName, CharacterClass characterClass) {
+        this.charName = charName;
+        this.characterClass = characterClass;
     }
+
+    public Character() {
+    }
+
+    public String getCharGender() {
+        return charGender;
+    }
+
+    public void setCharGender(String charGender) {
+        this.charGender = charGender;
+    }
+
+    public String getCharName() {
+        return charName;
+    }
+
+    public void setCharName(String charName) {
+        this.charName = charName;
+    }
+
+    public CharacterClass getCharacterClass() {
+        return characterClass;
+    }
+
+    public void setCharacterClass(CharacterClass characterClass) {
+        this.characterClass = characterClass;
+    }
+
+    public int getHitpoints() {
+        return hitpoints;
+    }
+
+    public void setHitpoints(int hitpoints) {
+        this.hitpoints = hitpoints;
+    }
+
+    public void printCharacterInfo() {
+        System.out.println(charName + ", " + characterClass.getType());
+    }
+
 }
--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -8,20 +8,26 @@ import javax.xml.parsers.SAXParserFactory;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        List<CharacterClass> classes;
+        List<ArrayList<String>> characterNames;
+        List<Character> characterList = new ArrayList<Character>();
+
         SAXParserFactory saxParserFactory = SAXParserFactory.newInstance();
         File namesFile = new File("src/main/resources/character_names.xml");
+
         try {
             SAXParser nameParser = saxParserFactory.newSAXParser();
             NameHandler nameHandler = new NameHandler();
             nameParser.parse(namesFile, nameHandler);
-            List<ArrayList<String>> names = nameHandler.getNamesList();
+            characterNames = nameHandler.getNamesList();
 
-            for (ArrayList<String> genderNames : names) {
+            for (ArrayList<String> genderNames : characterNames) {
                 for (String name : genderNames) {
                     System.out.println(name);
                 }
@@ -35,7 +41,7 @@ public class Main {
             File characterClassesFile = new File("src/main/resources/character_classes.xml");
             CharacterClassHandler characterClassHandler = new CharacterClassHandler();
             characterClassesParser.parse(characterClassesFile, characterClassHandler);
-            List<CharacterClass> classes = characterClassHandler.getClasses();
+            classes = characterClassHandler.getClasses();
 
             for (CharacterClass characterClass : classes) {
                 System.out.println("");
@@ -44,6 +50,38 @@ public class Main {
                 System.out.println("hitpoints: " + characterClass.getHitpoints());
                 System.out.println("mana: " + characterClass.getMana());
             }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            for (int i = 0; i < 5; i++) {
+                Random rnd = new Random();
+                Character character = new Character();
+                String gender;
+                switch (rnd.nextInt(3)) {
+                    case 0:
+                        gender = "male";
+                        break;
+                    case 1:
+                        gender = "female";
+                        break;
+                    case 2:
+                        gender = "neutral";
+                        break;
+                }
+                character.setCharGender(gender);
+                if (gender.contentEquals("male")) {
+                    character.setCharName(characterNames.get(0).get(rnd.nextInt(characterNames.get(0).size())));
+                } else if (gender.contentEquals("female")) {
+                    character.setCharName(characterNames.get(1).get(rnd.nextInt(characterNames.get(1).size())));
+                } else if (gender.contentEquals("neutral")) {
+                    character.setCharName(characterNames.get(2).get(rnd.nextInt(characterNames.get(2).size())));
+                }
+                character.setCharacterClass(classes.get(rnd.nextInt(classes.size())));
+                characterList.add(character);
+            }
+            int i = 1;
         } catch (Exception e) {
             e.printStackTrace();
         }
