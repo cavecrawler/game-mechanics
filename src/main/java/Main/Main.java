11f13321f0b4aa5233dd63ef1c89497bef4f71dd
cydietz <cy.dietz@googlemail.com>
2018-10-16 23:31:53 +0200

    - refactored CharacterGeneration out of Main.java

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -13,39 +13,8 @@ public class Main {
     public static void main(String[] args) {
 
 
-        XMLDataReader xmlReader = new XMLDataReader();
-        List<ArrayList<String>> characterNames = xmlReader.getCharacterNames();
-        List<CharacterClass> classes = xmlReader.getCharacterClasses();
-        List<Armor> armors = xmlReader.getArmorEquipment();
-        List<Weapon> weapons = xmlReader.getWeaponEquipment();
-        List<Character> characterList = new ArrayList<>();
-
-        for (int i = 0; i < 20; i++) {
-            Random rnd = new Random();
-            String gender = null;
-            switch (rnd.nextInt(2)) {
-                case 0:
-                    gender = "male";
-                    break;
-                case 1:
-                    gender = "female";
-                    break;
-            }
-            Character character = new Character(gender, classes.get(rnd.nextInt(classes.size())));
-            if (gender.contentEquals("male")) {
-                character.setName(characterNames.get(0).get(rnd.nextInt(characterNames.get(0).size())));
-            } else if (gender.contentEquals("female")) {
-                character.setName(characterNames.get(1).get(rnd.nextInt(characterNames.get(1).size())));
-            }
-
-            // try to equip an armor
-            character.equip(armors.get(0));
-
-            // try to equip a weapon
-            character.equip(weapons.get(0));
-
-            characterList.add(character);
-        }
+        CharacterGenerator characterGenerator = new CharacterGenerator();
+        List<Character> characterList = characterGenerator.getRandomCharacterList(10);
 
         for (Character character : characterList) {
             character.printCharacterInfo();
